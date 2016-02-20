f8406546a232d052c79b20f2a82c07a0c42b6c1f
Zent0 <zento@own-salvation.de>
2016-02-20 15:27:38 +0100

    Added files via upload

--- AmChartPHP.php
+++ AmChartPHP.php
@@ -22,118 +22,130 @@ class AmChartPHP
     //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     //SOFTWARE.
 	
-	protected $title = 'New Chart';
-	protected $type = 'serial';
-	protected $startDuration = 2;
-	protected $marginRight = 10;
-	protected $marginLeft = 10;
-	protected $marginTop = 10;
-	protected $marginBottom = 10;
-	protected $fontSize = 11;
-	protected $chartCursor = false;
+
+    protected $title = 'New Chart';
+    protected $type = 'serial';
+    protected $startDuration = 2;
+    protected $marginRight = 10;
+    protected $marginLeft = 10;
+    protected $marginTop = 10;
+    protected $marginBottom = 10;
+    protected $fontSize = 11;
+    protected $chartCursor = false;
 	
-	public $graphs;
-	function __construct()
-	{
-		$this->graphs = new stdClass();
-	}
-	
-	//Change title
-	function title($title)
-	{
-		$this->title = $title;
-	}
-	//Change Type of Chart
-	function type($type)
-	{
-		if(strtolower($type) == "serial")
-			$this->type = $type;
-		else
-			trigger_error ($type . ' is an unsupported chartType',E_USER_WARNING);
-	}
-	//Change duration of build up animation - INTEGER
-	function startDuration($startDuration)
-	{
-		if(is_integer($startDuration))
-			$this->startDuration = $startDuration;
-		else
-			trigger_error ('StartDuration must be integer',E_USER_WARNING);
-	}
-	//Change marginRight - INTEGER
-	function marginRight($marginRight)
-	{
-		if(is_integer($marginRight))
-			$this->marginRight = $marginRight;
-		else
-			trigger_error ('marginRight must be integer',E_USER_WARNING);
-	}
-	//Change marginLeft  - INTEGER
-	function marginLeft($marginLeft)
-	{
-		if(is_integer($marginLeft))
-			$this->marginLeft = $marginLeft;
-		else
-			trigger_error ('marginleft must be integer',E_USER_WARNING);
-	}
-	//Change marginTop  - INTEGER
-	function marginTop($marginTop)
-	{
-		if(is_integer($marginTop))
-			$this->marginTop = $marginTop;
-		else
-			trigger_error ('marginTop must be integer',E_USER_WARNING);
-	}
-	//Change marginBottom - INTEGER
-	function marginBottom($marginBottom)
-	{
-		if(is_integer($marginBottom))
-			$this->marginBottom = $marginBottom;
-		else
-			trigger_error ('marginBottom must be integer',E_USER_WARNING);
-	}
-	//Change fontSize - INTEGER
-	function fontSize($fontSize)
-	{
-		if(is_integer($fontSize))
-			$this->fontSize = $fontSize;
-		else
-			trigger_error ('fontSize must be integer',E_USER_WARNING);
-	}
-	//Enable / disable chartCursor - BOOLEAN
-	function chartCursor($chartCursor)
-	{
-		if(is_bool($chartCursor))
-			$this->chartCursor = $chartCursor;
-		else
-			trigger_error ('chartCursor must be integer',E_USER_WARNING);
-	}
-	//Creates a graph - STRING - ARRAY
-	function createGraph($name, $values)
-	{
-		if(isset($this->graphs->$name))
-			trigger_error ('Graph with this name already exist. Use ::deleteGraph to delete it first.',E_USER_ERROR);
-		else
-		{
-			$this->graphs->$name = new stdClass();
-			$this->graphs->$name->name = $name;
-			$this->graphs->$name->balloonText = "[[title]] of [[category]]:[[value]]";
-			$this->graphs->$name->bullet = 'round';
-			$this->graphs->$name->id = 'Graph-' - $name;
-			$this->graphs->$name->title = $name;
-			$this->graphs->$name->valueField = "value-" . $name;
-		}
-	}
-    //Deletes a graph - STRING
-	function deleteGraph($name)
-	{
-        if(isset($this->graphs->$name))
-            unset($this->graphs->$name);
-        else
-            trigger_error ('There is no chart with the name ' . $name . '.',E_USER_WARNING);
-	}
-    //Change BallonText of a chart - STRING - STRING
-    function GraphBallon()
+    public $graphs;
+    function __construct()
     {
-        
+    	$this->graphs = new stdClass();
+    }
+	
+    //Change title - STRING
+    function title($title)
+    {
+    	$this->title = $title;
+    }
+    //Change Type of Chart - STRING
+    function type($type)
+    {
+    	if(strtolower($type) == "serial")
+    		$this->type = $type;
+    	else
+    		trigger_error ($type . ' is an unsupported chartType',E_USER_WARNING);
+    }
+    //Change duration of build up animation - INTEGER
+    function startDuration(int $startDuration)
+    {
+    		$this->startDuration = $startDuration;
+    }
+    //Change marginRight - INTEGER
+    function marginRight(int $marginRight)
+    {
+  		$this->marginRight = $marginRight;
+    }
+    //Change marginLeft  - INTEGER
+    function marginLeft(int $marginLeft)
+    {
+    		$this->marginLeft = $marginLeft;
+
+    }
+    //Change marginTop  - INTEGER
+    function marginTop(int $marginTop)
+    {
+    	$this->marginTop = $marginTop;
+
+    }
+    //Change marginBottom - INTEGER
+    function marginBottom(int $marginBottom)
+    {
+    	$this->marginBottom = $marginBottom;
+    }
+    //Change fontSize - INTEGER
+    function fontSize(int $fontSize)
+    {
+    	$this->fontSize = $fontSize;
+    }
+    //Enable / disable chartCursor - BOOLEAN
+    function chartCursor($chartCursor)
+    {
+    	if(is_bool($chartCursor))
+    		$this->chartCursor = $chartCursor;
+    	else
+    		trigger_error ('chartCursor must be integer',E_USER_WARNING);
+    }
+    //Creates a graph - STRING - ARRAY
+    function createGraph($name, array $values)
+    {
+    	if(isset($this->graphs->$name))
+    		trigger_error ('Graph with this name already exist. Use ::deleteGraph to delete it first.',E_USER_ERROR);
+    	else
+    	{
+    		$this->graphs->$name = new stdClass();
+    		$this->graphs->$name->name = $name;
+    		$this->graphs->$name->balloonText = "[[title]] of [[category]]:[[value]]";
+    		$this->graphs->$name->bullet = 'round';
+    		$this->graphs->$name->id = 'Graph-' - $name;
+    		$this->graphs->$name->title = $name;
+           	$this->graphs->$name->valueField = "value-" . $name;
+            $this->graphs->$name->values = $values;
+        }
+    }
+    //Deletes a graph - STRING
+    function deleteGraph($name)
+    {
+        if($this->GraphExist($name))
+            unset($this->graphs->$name);
+    }
+    protected function GraphExist($name, $error = true)
+    {
+        if(isset($this->graphs->$name))
+            return true;
+        else
+        {
+            if($error)
+                trigger_error ('There is no chart with the name ' . $name . '.',E_USER_WARNING);
+            return false;
+        }
+    }
+    //Change BallonText of a graph - STRING - STRING
+    function GraphBallonText($name, $balloonText)
+    {
+        if($this->GraphExist($name))
+            $this->graphs->$name->balloonText = $balloonText;
+    }
+    //Change bullet of a graph - STRING - STRING
+    function GraphBullet($name, $bullet)
+    {
+        if($this->GraphExist($name))
+        {
+            $valid = array('none', 'round', 'triangleUp', 'triangleDown', 'triangleLeft', 'triangleRight', 'bubble', 'diamond', 'xError', 'yError', 'custom');
+            if(in_array($bullet, $valid))
+                $this->graphs->$name->bullet = $bullet;
+        }
+    }
+    //Change title of graph
+    function GraphTitle($name, $title)
+    {
+        if($this->GraphExist($name))
+            $this->graphs->$name->title = $title;
     }
 }
