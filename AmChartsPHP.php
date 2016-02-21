4fc06d3d1236b899f50ba3c8199f0b6eddb9950d
Zent0 <zento@own-salvation.de>
2016-02-21 17:30:38 +0100

    Added files via upload

--- AmChartsPHP.php
+++ AmChartsPHP.php
@@ -26,12 +26,22 @@ class AmChartPHP
     protected $title = 'New Chart';
     protected $type = 'serial';
     protected $startDuration = 2;
+	protected $autoMarginOffset = 20;
     protected $marginRight = 10;
     protected $marginLeft = 10;
     protected $marginTop = 10;
     protected $marginBottom = 10;
     protected $fontSize = 11;
-    protected $chartCursor = false;
+    protected $chartCursor = true;
+	protected $chartScrollbar = false;
+	protected $theme = 'light';
+	protected $legend = true;
+	protected $export = true;
+	protected $categoryAxisAutoRotateAngle = 45;
+	protected $categoryAxisAutoRotateCount = 10;
+	protected $categoryAxisGridPosition = "start";
+	protected $categoryAxisTitleRotation = 55;
+	protected $categoryValues = array();
 	
     public $graphs;
     function __construct()
@@ -52,10 +62,20 @@ class AmChartPHP
     	else
     		trigger_error ($type . ' is an unsupported chartType',E_USER_WARNING);
     }
+	//Set category Values - Array()
+	function categoryValues($categoryValues)
+	{
+		$this->categoryValues = $categoryValues;
+	}
     //Change duration of build up animation - INTEGER
     function startDuration(int $startDuration)
     {
     		$this->startDuration = $startDuration;
+    }
+	//Change autoMarginOffset - INTEGER
+    function autoMarginOffset(int $autoMarginOffset)
+    {
+  		$this->autoMarginOffset = $autoMarginOffset;
     }
     //Change marginRight - INTEGER
     function marginRight(int $marginRight)
@@ -84,29 +104,95 @@ class AmChartPHP
     {
     	$this->fontSize = $fontSize;
     }
+	//Change autoRotateAngel of categoryAxis - INTEGER
+	function categoryAxisAutoRotateAngle(int $categoryAxisAutoRotateAngle)
+	{
+		$this->categoryAxisAutoRotateAngle = $categoryAxisAutoRotateAngle;
+	}
+	//Change autoRotateCount of categoryAxis - INTEGER
+	function categoryAxisAutoRotateCount(int $categoryAxisAutoRotateCount)
+	{
+		$this->categoryAxisAutoRotateCount = $categoryAxisAutoRotateCount;
+	}
+	//Change gridPosition of categoryAxis - INTEGER
+	function categoryAxisGridPosition(int $categoryAxisGridPosition)
+	{
+		$this->categoryAxisGridPosition = $categoryAxisGridPosition;
+	}
+	//Change titleRotation of categoryAxis - INTEGER
+	function categoryAxisTitleRotation(int $categoryAxisTitleRotation)
+	{
+		$this->categoryAxisTitleRotation = $categoryAxisTitleRotation;
+	}
     //Enable / disable chartCursor - BOOLEAN
     function chartCursor($chartCursor)
     {
     	if(is_bool($chartCursor))
     		$this->chartCursor = $chartCursor;
     	else
-    		trigger_error ('chartCursor must be integer',E_USER_WARNING);
+    		trigger_error ('chartCursor must be BOOLEAN',E_USER_WARNING);
     }
-    //Creates a graph - STRING - ARRAY
-    function createGraph($name, array $values)
+	//Enable / disable chartScrollbar - BOOLEAN
+    function chartScrollbar($chartScrollbar)
+    {
+    	if(is_bool($chartScrollbar))
+    		$this->chartScrollbar = $chartScrollbar;
+    	else
+    		trigger_error ('chartScrollbar must be BOOLEAN',E_USER_WARNING);
+    }
+	//Enable / disable legend - BOOLEAN
+    function legend($legend)
+    {
+    	if(is_bool($legend))
+    		$this->legend = $legend;
+    	else
+    		trigger_error ('legend must be BOOLEAN',E_USER_WARNING);
+    }
+	//Enable / disable export - BOOLEAN
+    function export($export)
+    {
+    	if(is_bool($export))
+    		$this->export = $export;
+    	else
+    		trigger_error ('export must be BOOLEAN',E_USER_WARNING);
+    }
+	//Change theme - STRING
+	function theme($theme)
+	{
+		$valid = array('none', 'light', 'dark', 'black', 'patterns', 'chalk');
+        if(in_array($theme, $valid))
+            $this->theme = $theme;
+	}
+    //Creates a graph - STRING - STRING - ARRAY
+    function createGraph($name, $type, array $values)
     {
     	if(isset($this->graphs->$name))
     		trigger_error ('Graph with this name already exist. Use ::deleteGraph to delete it first.',E_USER_ERROR);
     	else
     	{
-    		$this->graphs->$name = new stdClass();
-    		$this->graphs->$name->name = $name;
-    		$this->graphs->$name->balloonText = "[[title]] of [[category]]:[[value]]";
-    		$this->graphs->$name->bullet = 'round';
-    		$this->graphs->$name->id = 'Graph-' - $name;
-    		$this->graphs->$name->title = $name;
-           	$this->graphs->$name->valueField = "value-" . $name;
-            $this->graphs->$name->values = $values;
+            $this->graphs->$name = new stdClass();
+            $this->graphs->$name->name = $name;
+			$this->graphs->$name->type = $type;
+            $this->graphs->$name->balloonText = "[[title]] of [[category]]:[[value]]";
+            $this->graphs->$name->bullet = 'none';
+			$this->graphs->$name->bulletSize = 6;
+            $this->graphs->$name->id = 'Graph-' - $name;
+            $this->graphs->$name->title = $name;
+            $this->graphs->$name->valueField = "value-" . $name;
+			$this->graphs->$name->dashLength = 0;
+			if($type == 'line' OR $type == 'smoothedLine')
+				$this->graphs->$name->lineAlpha = 1;
+			else
+				$this->graphs->$name->lineAlpha = 0;
+			$this->graphs->$name->lineColor = "#AA0000";
+			$this->graphs->$name->lineThickness = 2.5;
+			if($type == 'line' OR $type == 'smoothedLine')
+				$this->graphs->$name->fillAlphas = 0;
+			else
+				$this->graphs->$name->fillAlphas = 1;
+			$this->graphs->$name->fillColors = "#AA0000";
+			
+			$this->graphs->$name->values = $values;
         }
     }
     //Deletes a graph - STRING
@@ -115,7 +201,8 @@ class AmChartPHP
         if($this->GraphExist($name))
             unset($this->graphs->$name);
     }
-    protected function GraphExist($name, $error = true)
+    //Checks if a graph with this name already exists. - STRING - BOOL
+    function GraphExist($name, $error = true)
     {
         if(isset($this->graphs->$name))
             return true;
@@ -142,10 +229,133 @@ class AmChartPHP
                 $this->graphs->$name->bullet = $bullet;
         }
     }
-    //Change title of graph
+	//Change bulletSize of graph - STRING - INTEGER
+    function GraphBulletSize($name, int $bulletSize)
+    {
+        if($this->GraphExist($name))
+            $this->graphs->$name->bulletSize = $bulletSize;
+    }
+	//Change dashLength of graph - STRING - INTEGER
+    function GraphDashLength($name, int $dashLength)
+    {
+        if($this->GraphExist($name))
+            $this->graphs->$name->dashLength = $dashLength;
+    }
+	//Change lineAlpha of graph - STRING - DOUBLE
+    function GraphLineAlpha($name, $lineAlpha)
+    {
+        if($this->GraphExist($name))
+            $this->graphs->$name->lineAlpha = $lineAlpha;
+    }
+    //Change title of graph - STRING - STRING
     function GraphTitle($name, $title)
     {
         if($this->GraphExist($name))
             $this->graphs->$name->title = $title;
     }
+	//Change lineColor of graph - STRING - STRING (Color Code)
+    function GraphLineColor($name, $lineColor)
+    {
+        if($this->GraphExist($name))
+            $this->graphs->$name->lineColor = $lineColor;
+    }
+	//Change lineThickness of graph - STRING - DOUBLE
+    function GraphLineThickness($name, $lineThickness)
+    {
+        if($this->GraphExist($name))
+            $this->graphs->$name->lineThickness = $lineThickness;
+    }
+	//Change fillAlphas of graph - STRING - DOUBLE
+    function GraphFillAlphas($name, $fillAlphas)
+    {
+        if($this->GraphExist($name))
+            $this->graphs->$name->fillAlphas = $fillAlphas;
+    }
+	//Change fillColors of graph - STRING - STRING (Color Code)
+    function GraphFillColors($name, $fillColors)
+    {
+        if($this->GraphExist($name))
+            $this->graphs->$name->fillColors = $fillColors;
+    }
+	//Creates the chart in div - STRING
+	function chart($div)
+	{
+		echo '<script type="text/javascript">';
+			echo 'AmCharts.makeChart("' . $div . '",
+				{';
+				echo '"type": "' . $this->type . '",';
+				echo '"categoryField": "category",';
+				echo '"autoMarginOffset": ' . $this->autoMarginOffset . ',';
+				echo '"marginRight": ' . $this->marginRight . ',';
+				echo '"marginLeft": ' . $this->marginLeft . ',';
+				echo '"marginTop": ' . $this->marginTop . ',';
+				echo '"marginBottom": ' . $this->marginBottom . ',';
+				echo '"startDuration": ' . $this->startDuration . ',';
+				echo '"fontSize": ' . $this->fontSize . ',';
+				echo '"theme": "' . $this->theme . '",';
+				if($this->chartCursor)
+					echo '"chartCursor": {},';
+				if($this->chartScrollbar)
+					echo '"chartScrollbar": {},';
+				if($this->legend)
+					echo '"legend": {
+							"enabled": true,
+							"useGraphSettings": true
+						},';
+				if($this->export)
+					echo '"export":
+							{
+								"enabled":true		
+							},';
+				echo '"categoryAxis":
+						{
+							"autoRotateAngle": ' . $this->categoryAxisAutoRotateAngle . ',
+							"autoRotateCount": ' . $this->categoryAxisAutoRotateCount . ',
+							"gridPosition": "' . $this->categoryAxisGridPosition . '",
+							"titleRotation": ' . $this->categoryAxisTitleRotation . '
+						},';
+				echo '"graphs": [';
+					foreach($this->graphs as $g)
+					{
+						echo '{';
+							echo '"balloonText": "' . $g->balloonText . '",';
+							echo '"bullet": "' . $g->bullet . '",';
+							echo '"bulletSize": ' . $g->bulletSize . ',';
+							echo '"id": "' . $g->id . '",';
+							echo '"title": "' . $g->title . '",';
+							echo '"valueField": "' . $g->valueField . '",';
+							echo '"dashLength": ' . $g->dashLength . ',';
+							echo '"lineAlpha": ' . $g->lineAlpha . ',';
+							echo '"lineColor": "' . $g->lineColor . '",';
+							echo '"lineThickness": ' . $g->lineThickness . ',';
+							echo '"fillAlphas": ' . $g->fillAlphas . ',';
+							echo '"fillColors": "' . $g->fillColors . '",';
+							echo '"type": "' . $g->type . '",';
+						echo '},';
+					}
+				echo '],';
+				echo '"valueAxes": [
+						{
+							"id": "ValueAxis-1",
+							"title": "Axis title"
+						}
+					],';
+				echo '"dataProvider": [';
+					$i = 0;
+					foreach($this->categoryValues as $c)
+					{
+						echo '{';
+							echo '"category": ' . $c . ',';
+							foreach($this->graphs as $g)
+							{
+								echo '"' . $g->valueField . '": ' . $g->values[$i] . ',';
+								//echo '"value-bla": ' . $c . ',';
+							}
+						echo '},';
+						$i++;
+					}
+				echo ']';
+			echo '});';
+		echo '</script>';
+	}
 }
